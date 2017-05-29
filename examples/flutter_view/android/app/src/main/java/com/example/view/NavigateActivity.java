0c255b59f4f2f044995860db34e5033b052d91bf
Sarah Zakarias <zarah@google.com>
2017-05-29 15:49:12 +0200

    played around with routes

---
+++ examples/flutter_view/android/app/src/main/java/com/example/view/NavigateActivity.java
@@ -0,0 +1,45 @@
+package com.example.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+
+
+public class NavigateActivity extends FlutterActivity {
+
+    private static final String CHANNEL = "samples.flutter.io/platform_view";
+    private static final String METHOD_SWITCH_VIEW = "switchView";
+    //private static final int COUNT_REQUEST = 1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+                new MethodChannel.MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+                       // MainActivity.this.result = result;
+                        //int count = methodCall.arguments();
+                        if (methodCall.method.equals(METHOD_SWITCH_VIEW)) {
+                            onlaunchSplitScreen();
+                        } else {
+                            result.notImplemented();
+                        }
+                    }
+                }
+        );
+    }
+
+
+    private void onlaunchSplitScreen() {
+        Intent splitScreenIntent = new Intent(this, MainActivity.class);
+
+        startActivity(splitScreenIntent);
+    }
+
+
+}
